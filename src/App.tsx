f380d55b94af8038be5619b34ebc418b09eb5154
HarryAmmon <harry_a99@hotmail.co.uk>
2020-06-29 10:24:14 +0100

    Using CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import Styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={Styles.App}>
+      <header className={Styles.AppHeader}>
+        <img src={logo} className={Styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={Styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <p className={Styles.RedBorder}>Styled with CSS Modules</p>
       </header>
     </div>
   );
