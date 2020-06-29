7b15287c97f976d68a8e863cbab2fb10c489b74f
HarryAmmon <harry_a99@hotmail.co.uk>
2020-06-29 15:49:12 +0100

    Updates

---
+++ precache-manifest.36dbf1f9783f887021872ffc297b587c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19013a1ac40badd871ca9f432c12342",
+    "url": "/userresearch/index.html"
+  },
+  {
+    "revision": "cde475ace6d371f044f7",
+    "url": "/userresearch/static/css/main.41b4e322.chunk.css"
+  },
+  {
+    "revision": "2972beb2fcf468caf208",
+    "url": "/userresearch/static/js/2.a685f12b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userresearch/static/js/2.a685f12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde475ace6d371f044f7",
+    "url": "/userresearch/static/js/main.38d294bf.chunk.js"
+  },
+  {
+    "revision": "af69826e43dea0a447ab",
+    "url": "/userresearch/static/js/runtime-main.e75d416b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/userresearch/static/media/logo.ee7cd8ed.svg"
+  }
+]);
