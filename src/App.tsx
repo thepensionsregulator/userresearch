fd79c36abb3709d7b00b5efeb5def3c77a12b682
HarryAmmon <harry_a99@hotmail.co.uk>
2020-06-30 16:14:55 +0100

    Adding global components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import Styles from './App.module.scss';
+import ThemeProvider from '@tpr/theming';
+import { Header, Footer, Highlight } from '@tpr/layout';
 
 function App() {
   return (
-    <div className={Styles.App}>
-      <header className={Styles.AppHeader}>
-        <img src={logo} className={Styles.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={Styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p className={Styles.RedBorder}>Testing GitHub actions small change</p>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Header
+        logoUrl={
+          'https://www.thepensionsregulator.gov.uk/-/media/thepensionsregulator/images/logo/tpr-logo-header.ashx'
+        }
+        title={'User Research'}
+        onClickSchemeOptions={() => {
+          console.log('Scheme Options clicked');
+        }}
+        onClickLogout={() => {
+          console.log('Logout clicked');
+        }}
+      />
+      <Highlight name={'Scheme Name'} psr={'012161'} />
+      <Footer
+        logoUrl="https://www.thepensionsregulator.gov.uk/-/media/thepensionsregulator/images/logo/tpr-logo-footer.ashx"
+        onLinkClickHandler={(url) => console.log(`navigate to ${url}`)}
+        links={[
+          { title: 'Website help', url: 'link-1' },
+          { title: 'Cymraeg', url: 'link-2' },
+          { title: 'Site map', url: 'link-3' },
+        ]}
+      />
+    </ThemeProvider>
   );
 }
 
