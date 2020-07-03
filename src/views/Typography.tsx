d2beb59d9398ffb0b75dea46ace302f71ce68168
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-03 12:23:55 +0100

    Removing unused imports

--- src/views/Typography.tsx
+++ src/views/Typography.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { H1, H2, H3, H4, H5, H6, P, Span, Text, Hr } from '@tpr/core';
-import {
-  FFCheckbox,
-  Form,
-  FFInputDate,
-  FFInputNumber,
-  FFRadioButton,
-} from '@tpr/forms';
+import { H1, H2, H3, H4, H5, H6, P, Span, Text, Hr, Button } from '@tpr/core';
 import Styles from './Typography.module.scss';
 import TextInputExample from '../components/TextInputExample';
 import SelectInputExample from '../components/SelectInputExample';
