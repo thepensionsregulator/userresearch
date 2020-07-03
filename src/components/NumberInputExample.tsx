a3dc2d0a913f7943ef54429b79ad13bca801c077
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-03 12:22:39 +0100

    Adding another number example

--- src/components/NumberInputExample.tsx
+++ src/components/NumberInputExample.tsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Form, FFInputNumber } from '@tpr/forms';
+import { P } from '@tpr/core';
+import Styles from './Spacing.module.scss';
 
 const NumberInputExample = () => (
-  <Form onSubmit={console.log}>
-    {({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <FFInputNumber
-          required
-          name="DoB entry"
-          label="What is your favorite number?"
-        />
-      </form>
-    )}
-  </Form>
+  <div>
+    <P>Number inputs can be optional</P>
+    <Form onSubmit={console.log}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <FFInputNumber
+            required
+            name="FavNumberEntry"
+            label="What is your favorite number?"
+          />
+        </form>
+      )}
+    </Form>
+    <P className={Styles.root}>Number inputs can have validation</P>
+    <Form onSubmit={console.log}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <FFInputNumber
+            required
+            name="FavNumberEntry"
+            label="What is your favorite number?"
+            hint="Your favorite number must be between 0 and 9"
+            validate={(value) => {
+              if (value < 0 || value > 9 || (!value && value !== 0)) {
+                return 'Must be between 0 and 9';
+              }
+            }}
+          />
+        </form>
+      )}
+    </Form>
+  </div>
 );
 
 export default NumberInputExample;
