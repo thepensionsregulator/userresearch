6f8bd639b96281a64b1e2d1d904a5806ea7aa741
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-03 12:22:24 +0100

    Adding another Date example

--- src/components/DateInputExample.tsx
+++ src/components/DateInputExample.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Form, FFInputDate } from '@tpr/forms';
+import { P } from '@tpr/core';
+import Styles from './Spacing.module.scss';
 
 const DateInputExample = () => (
-  <Form onSubmit={console.log}>
-    {({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <FFInputDate required name="DoB entry" label="When were you born?" />
-      </form>
-    )}
-  </Form>
+  <div>
+    <Form onSubmit={console.log}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <FFInputDate required name="DoB entry" label="When were you born?" />
+        </form>
+      )}
+    </Form>
+    <P className={Styles.root}>Date fields can be optional</P>
+    <Form onSubmit={console.log}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <FFInputDate name="DoB entry" label="When were you born?" />
+        </form>
+      )}
+    </Form>
+  </div>
 );
 
 export default DateInputExample;
