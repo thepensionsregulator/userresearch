00b1b5fc0da180f65100252f0d3c5e3f6f3788b2
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-07 16:29:06 +0100

    Adding components for initial-questions

--- src/views/InitialQuestions.tsx
+++ src/views/InitialQuestions.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import Styles from './Layout.module.scss';
+import { ArrowLink, ArrowButton } from '@tpr/layout';
+import { H1, Hr, P } from '@tpr/core';
+import { Form, renderFields, FieldProps } from '@tpr/forms';
 
-const InitialQuestions = () => (
-  <div>
-    <h1>InitialQuestions</h1>
-  </div>
-);
-
+const InitialQuestions = () => {
+  const FIELD_NAME = 'schemeWoundUp?';
+  const fields: FieldProps[] = [
+    {
+      name: FIELD_NAME,
+      label: 'Yes',
+      value: true,
+      type: 'radio',
+      cfg: { my: 4 },
+    },
+    { name: FIELD_NAME, label: 'No', value: false, type: 'radio' },
+  ];
+  return (
+    <div className={Styles.root}>
+      <div className={Styles.main}>
+        <ArrowLink
+          iconSide="left"
+          pointsTo="left"
+          onClick={() => console.log('clickity click')}
+        />
+        <H1>Check if you need to complete a scheme return</H1>
+        <Hr cfg={{ my: 2 }} />
+        <P cfg={{ my: 4 }}>
+          <b>Has this pension scheme been wound up and no longer exists?</b>
+        </P>
+        <P>
+          You should only choose 'yes' if the scheme no longer has any members,
+          assets or liabilities and the winding up
+        </P>
+        <P>
+          process has finished. If you're not sure, check the current details of
+          the scheme. (open in new window/tab)
+        </P>
+        <Form onSubmit={console.log}>
+          {({ handleSubmit }) => (
+            <form>
+              {renderFields(fields)}
+              <Hr cfg={{ my: 8 }} />
+              <ArrowButton
+                iconSide="right"
+                pointsTo="right"
+                onClick={() => console.log('click again')}
+                title={'Continue'}
+                disabled={true}
+              />
+            </form>
+          )}
+        </Form>
+        <div className={Styles.backToTop}>
+          <ArrowLink
+            iconSide="left"
+            pointsTo="up"
+            onClick={() => console.log('clicked')}
+            cfg={{ mr: 3 }}
+            title="Back to top"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default InitialQuestions;
