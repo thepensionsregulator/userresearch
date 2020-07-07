c42a8cb0c3d1cb2975b046b7ccaa79879d50c987
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-07 10:52:48 +0100

    Adding margins & BackToTop link

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { H1, Hr, H2, P } from '@tpr/core';
-import { Sidebar, Info } from '@tpr/layout';
+import { Sidebar, Info, ArrowLink } from '@tpr/layout';
 import Styles from './Layout.module.scss';
 
 const HomePage = () => (
   <div className={Styles.root}>
-    <div className={Styles.sidebar}>
+    <div>
       <Sidebar />
     </div>
     <div className={Styles.main}>
-      <H1>Scheme Return</H1>
-      <Hr cfg={{ my: 5 }} />
+      <H1 cfg={{ mb: 6 }}>Scheme Return</H1>
+      <Hr cfg={{ mb: 10 }} />
       <Info title="Due Date" importantMessage="Important">
-        The scheme return for this scheme is due. <br /> You must submit the
-        completed scheme return by 31 March 2021.
+        <P cfg={{ mb: 1 }}>The scheme return for this scheme is due.</P>
+        <P cfg={{ mt: 2 }}>
+          You must submit the completed scheme return by{' '}
+          <strong>31 March 2021</strong>.
+        </P>
       </Info>
-      <Hr cfg={{ my: 5 }} />
-      <H2>Before you start</H2>
-      <P cfg={{ my: 3 }}>
+      <Hr cfg={{ mt: 6 }} />
+      <H2 cfg={{ mt: 9 }}>Before you start</H2>
+      <P cfg={{ my: 4 }}>
         Make sure you have the following information to hand:
       </P>
       <ul className={Styles.list}>
@@ -32,6 +35,15 @@ const HomePage = () => (
         </li>
       </ul>
       <Hr cfg={{ my: 5 }} />
+      <div className={Styles.backToTop}>
+        <ArrowLink
+          iconSide="left"
+          pointsTo="up"
+          onClick={() => console.log('clicked')}
+          cfg={{ mr: 3 }}
+          title="Back to top"
+        />
+      </div>
     </div>
   </div>
 );
