88ee67e38ac48d8c11d229677669227b97a3a924
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-08 15:57:28 +0100

    Adding margins and BackToTop button

--- src/views/SchemeWoundUp.tsx
+++ src/views/SchemeWoundUp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArrowLink, WarningBox, ArrowButton } from '@tpr/layout';
 import { H1, Hr, P, Link } from '@tpr/core';
-import { Form, FieldProps, renderFields } from '@tpr/forms';
+import { Form, FieldProps, renderFields, validate } from '@tpr/forms';
 import Styles from './Layout.module.scss';
 import { useHistory } from 'react-router-dom';
 
@@ -18,6 +18,7 @@ const SchemeWoundUp = () => {
       hint: 'For example, 31 3 2019 or 31 03 2019',
       error: 'Date is required',
       required: true,
+      cfg: { mb: 6 },
     },
   ];
   return (
@@ -30,39 +31,56 @@ const SchemeWoundUp = () => {
         />
         <H1 cfg={{ mb: 8 }}>Scheme wound up</H1>
         <Hr cfg={{ mb: 8 }} />
-        <P>
+        <P cfg={{ mb: 4 }}>
           If this scheme has wound up, you don't need to complete a scheme
           return.
         </P>
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit={onSubmit} validate={validate(fields)}>
           {({ handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               {renderFields(fields)}
-              <WarningBox>
-                <P>
-                  <b>Are you sure the scheme has wound up?</b>
-                </P>
-                <P>
-                  Choosing 'Confirm' will update the details we hold about this
-                  scheme and will remove it from your list of schemes. You'll no
-                  longer be able to access it. You may want to save or print
-                  previous scheme returns for your records.
-                </P>
-                <ArrowButton
-                  intent="danger"
-                  title="Confirm"
-                  pointsTo="right"
-                  iconSide="right"
-                />
-                <Link underline={true}>Cancel</Link>
-              </WarningBox>
+              <div className={Styles.warningBox}>
+                <WarningBox>
+                  <P cfg={{ my: 3 }}>
+                    <b>Are you sure this scheme has wound up?</b>
+                  </P>
+                  <P cfg={{ mb: 4 }}>
+                    Choosing 'Confirm' will update the details we hold about
+                    this scheme and will remove it from your list of schemes.
+                    You'll no longer be able to access it. You may want to{' '}
+                    <Link underline>save or print previous scheme returns</Link>
+                    for your records.
+                  </P>
+                  <div>
+                    <ArrowButton
+                      type="submit"
+                      intent="danger"
+                      title="Confirm"
+                      pointsTo="right"
+                      iconSide="right"
+                      cfg={{ mr: 4 }}
+                    />
                     <Link
                       underline
                       onClick={() => history.push('/initial-questions')}
                     >
+                      Cancel
+                    </Link>
+                  </div>
+                </WarningBox>
+              </div>
             </form>
           )}
         </Form>
+        <div className={Styles.backToTop}>
+          <ArrowLink
+            iconSide="left"
+            pointsTo="up"
+            onClick={() => console.log('clicked')}
+            cfg={{ mr: 3 }}
+            title="Back to top"
+          />
+        </div>
       </div>
     </div>
   );
