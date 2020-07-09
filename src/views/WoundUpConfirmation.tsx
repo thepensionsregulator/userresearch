d010804d1c2faae332164a37e7eb52fa9b6434d7
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-09 16:04:54 +0100

    Removing some components
    Was looking at the wrong Hi-Fi design, oops

--- src/views/WoundUpConfirmation.tsx
+++ src/views/WoundUpConfirmation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from './Layout.module.scss';
-import { H1, H2, P, Hr, Link } from '@tpr/core';
+import { H1, H4, P, Hr, Link } from '@tpr/core';
 import { Info, ArrowButton, ArrowLink } from '@tpr/layout';
 
 const WoundUpConfirmation = () => {
@@ -8,48 +8,22 @@ const WoundUpConfirmation = () => {
     <div className={Styles.root}>
       <div className={Styles.main}>
         <Info>
-          <H1>Scheme return complete</H1>
+          <H1>Scheme wound up confirmed</H1>
           <P>
-            You successfully submitted your scheme return to The Pensions
-            Regulator on <b>3 January 2020</b>.
+            You confirmed on <b>9 July 2020</b> that your scheme has now wound
+            up.
           </P>
           <P>Thank you for your time and co-operation.</P>
         </Info>
         <div>
           <ArrowButton
-            title="Return to Scheme options"
+            title="Return to My Schemes"
             iconSide="right"
             pointsTo="right"
-            cfg={{ mr: 4 }}
+            cfg={{ mr: 4, mt: 7 }}
           ></ArrowButton>
-          <Link underline>Download and print options</Link>
         </div>
-        <Hr />
-        <H2>What happens next</H2>
-        <P>
-          It is the trustee's responsibility to inform The Pensions Regulator of
-          any changes to scheme information.
-        </P>
-        <P>
-          In the past, you may have used form PR10 to let us know of any
-          changes, for example:
-        </P>
-        <ul>
-          <li>changes to the name and/or address of the scheme;</li>
-          <li>change of employer or trustee;</li>
-          <li>change to the scheme's status; or</li>
-          <li>change to the number of scheme members.</li>
-        </ul>
-        <P>
-          However, you can now access this online scheme maintenance system at
-          any time, enabling you to update your scheme information with changes
-          as and when they happen. You no longer need to complete a form PR10.
-        </P>
-        <Hr />
-        <div>
-          <Link>What did you think of this service?</Link>
-          <P>(takes 30 seconds)</P>
-        </div>
+        <Hr cfg={{ my: 4 }} />
         <div className={Styles.backToTop}>
           <ArrowLink
             iconSide="left"
