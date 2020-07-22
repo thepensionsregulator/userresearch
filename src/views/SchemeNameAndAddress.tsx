cbbd27ccdf7d3359f83e8485cd6e40524541943c
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-22 10:24:39 +0100

    Creating SchemeNameAndAddress.tsx

---
+++ src/views/SchemeNameAndAddress.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { H1 } from '@tpr/core';
+
+const SchemeNameAndAddress = () => {
+  return <H1>Scheme name and address</H1>;
+};
+
+export default SchemeNameAndAddress;
