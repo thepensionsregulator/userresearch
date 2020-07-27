6235acbc78c8a7829abebe7c743b7478dbfd1bae
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-27 17:01:36 +0100

    Adding link to /add-trustee

--- src/components/AddTrustee.tsx
+++ src/components/AddTrustee.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import {Link} from '@tpr/core';
-import {useHistory} from 'react-router-dom';
+import { Link } from '@tpr/core';
+import { useHistory } from 'react-router-dom';
 
-
-const AddTrustee = () =>{ 
-    const history =  useHistory(); 
-    return(
-        <Link onClick={() => {history.push('/trustees/add-trustee')}}>
-            + Add trustee
-        </Link>
-        );
-    }
+const AddTrustee = () => {
+  const history = useHistory();
+  return (
+    <Link
+      onClick={() => {
+        history.push('/trustees/add-trustee');
+      }}
+    >
+      + Add trustee
+    </Link>
+  );
+};
 
 export default AddTrustee;
