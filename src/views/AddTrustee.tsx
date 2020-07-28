8c450c184303354849b0f8188c26a0262718fbe8
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-28 10:15:33 +0100

    Adds margins to AddTrustee

--- src/views/AddTrustee.tsx
+++ src/views/AddTrustee.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from './Layout.module.scss';
-import { H1, H4, H3, Hr } from '@tpr/core';
+import { H1, H4, H3, Hr, Flex } from '@tpr/core';
 import UserResearchSidebar from '../components/UserResearchSidebar';
 import { ArrowLink, ArrowButton } from '@tpr/layout';
 import { useHistory } from 'react-router-dom';
@@ -39,19 +39,23 @@ const AddTrustee = () => {
           pointsTo="left"
           title="Back"
           onClick={() => {
-            history.push('/trustees');
+            history.goBack();
           }}
+          cfg={{ mt: 3 }}
         />
-        <H3>Add trustee: Individual</H3>
-        <H1>Type of trustee</H1>
-        <Hr />
-        <H4>Add an individual or corporate trustee?</H4>
+        <H3 cfg={{ mt: 2 }}>Add trustee: Individual</H3>
+        <H1 cfg={{ mt: 2, mb: 3 }}>Type of trustee</H1>
+        <Hr cfg={{ mt: 4, mb: 5 }} />
+        <H4 cfg={{ mb: 6 }}>Add an individual or corporate trustee?</H4>
         <Form onSubmit={submit}>
           {({ handleSubmit }) => (
             <form onSubmit={handleSubmit}>
-              <SeparatorX>{renderFields(fields)}</SeparatorX>
-              <Hr />
+              <Flex cfg={{ mt: 2, mb: 4 }}>
+                <SeparatorX>{renderFields(fields)}</SeparatorX>
+              </Flex>
+              <Hr cfg={{ mt: 8, mb: 4 }} />
               <ArrowButton
+                cfg={{ my: 4 }}
                 pointsTo="right"
                 iconSide="right"
                 type="submit"
