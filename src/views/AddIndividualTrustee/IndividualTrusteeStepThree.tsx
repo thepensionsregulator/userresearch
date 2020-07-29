793885659b7c71357311f0f4d26c5dcc0346f686
HarryAmmon <harry_a99@hotmail.co.uk>
2020-07-29 10:44:52 +0100

    Adds fields for step-3

--- src/views/AddIndividualTrustee/IndividualTrusteeStepThree.tsx
+++ src/views/AddIndividualTrustee/IndividualTrusteeStepThree.tsx
@@ -1,12 +1,75 @@
 import React from 'react';
 import Styles from '../Layout.module.scss';
 import UserResearchSidebar from '../../components/UserResearchSidebar';
-import { ArrowLink } from '@tpr/layout';
+import { ArrowLink, ArrowButton } from '@tpr/layout';
 import { useHistory } from 'react-router-dom';
-import { H3 } from '@tpr/core';
+import { H3, H1, Hr } from '@tpr/core';
+import { Form, renderFields, FieldProps } from '@tpr/forms';
+import ScrollToTop from '../../components/ScrollToTop';
 
 const IndividualTrusteeStepThree = () => {
   const history = useHistory();
+
+  const submit = () => {
+    console.log('Submitting....');
+  };
+
+  const fields: FieldProps[] = [
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'addressLine1',
+      label: 'Address line 1',
+      cfg: { my: 5 },
+      validate: (values) => {
+        if (!values) return 'Enter address line 1';
+      },
+    },
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'addressLine2',
+      label: 'Address line 2',
+      cfg: { my: 5 },
+      validate: (_) => {},
+    },
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'addressLine3',
+      label: 'Address line 3',
+      cfg: { my: 5 },
+      validate: (_) => {},
+    },
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'postTown',
+      label: 'Post town',
+      cfg: { my: 5 },
+      validate: (values) => {
+        if (!values) return 'Enter post town';
+      },
+    },
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'county',
+      label: 'County',
+      cfg: { my: 5 },
+      validate: (_) => {},
+    },
+    {
+      type: 'text',
+      inputWidth: 5,
+      name: 'postCode',
+      label: 'Post code',
+      cfg: { my: 5 },
+      validate: (values) => {
+        if (!values) return 'Enter postcode';
+      },
+    },
+  ];
   return (
     <div className={Styles.root}>
       <UserResearchSidebar />
@@ -18,6 +81,25 @@ const IndividualTrusteeStepThree = () => {
           onClick={() => history.goBack()}
         />
         <H3>Add trustee: Individual</H3>
+        <H1>What is this trustee's address?</H1>
+        <Hr cfg={{ mt: 4, mb: 5 }} />
+        <Form onSubmit={submit}>
+          {({ handleSubmit, valid }) => (
+            <form onSubmit={handleSubmit}>
+              {renderFields(fields)}
+              <Hr cfg={{ mt: 8, mb: 4 }} />
+              <ArrowButton
+                type="submit"
+                iconSide="right"
+                pointsTo="right"
+                title="Continue"
+                cfg={{ my: 4 }}
+                disabled={!valid}
+              />
+            </form>
+          )}
+        </Form>
+        <ScrollToTop />
       </div>
     </div>
   );
