52fc8fc5a93878a442d3ff8d2bbd6cefdd6487ce
HarryAmmon <harry_a99@hotmail.co.uk>
2020-08-04 14:30:30 +0100

    Pass useState values to children components

--- src/views/AddTrustee.tsx
+++ src/views/AddTrustee.tsx
@@ -6,28 +6,56 @@ import IndividualTrusteeStepOne from './AddIndividualTrustee/IndividualTrusteeSt
 import IndividualTrusteeStepTwo from './AddIndividualTrustee/IndividualTrusteeStepTwo';
 import IndividualTrusteeStepThree from './AddIndividualTrustee/IndividualTrusteeStepThree';
 import IndividualTrusteeStepZero from './AddIndividualTrustee/IndividualTrusteeStepZero';
+import { TrusteeInput } from '@tpr/layout/lib/components/cards/trustee/context';
 
 const AddTrustee = () => {
   const [state, setState] = useState<
     'step-0' | 'step-1' | 'step-2' | 'step-3' | 'step-4'
   >('step-0');
 
+  const [newTrustee, setNewTrustee] = useState<TrusteeInput>();
+
   const renderForm = () => {
     switch (state) {
       case 'step-0':
         return (
-          <IndividualTrusteeStepZero onSubmit={() => setState('step-1')} />
+          <IndividualTrusteeStepZero
+            nextPage={() => setState('step-1')}
+            newTrustee={newTrustee}
+            setNewTrustee={setNewTrustee}
+          />
         );
       case 'step-1':
-        return <IndividualTrusteeStepOne onSubmit={() => setState('step-2')} />;
+        return (
+          <IndividualTrusteeStepOne
+            nextPage={() => setState('step-2')}
+            setNewTrustee={setNewTrustee}
+            newTrustee={newTrustee}
+          />
+        );
       case 'step-2':
-        return <IndividualTrusteeStepTwo onSubmit={() => setState('step-3')} />;
+        return (
+          <IndividualTrusteeStepTwo
+            nextPage={() => setState('step-3')}
+            setNewTrustee={setNewTrustee}
+            newTrustee={newTrustee}
+          />
+        );
       case 'step-3':
         return (
-          <IndividualTrusteeStepThree onSubmit={() => setState('step-4')} />
+          <IndividualTrusteeStepThree
+            nextPage={() => setState('step-4')}
+            setNewTrustee={setNewTrustee}
+            newTrustee={newTrustee}
+          />
         );
       case 'step-4':
-        return <IndividualTrusteeStepFour />;
+        return (
+          <IndividualTrusteeStepFour
+            setNewTrustee={setNewTrustee}
+            newTrustee={newTrustee}
+          />
+        );
     }
   };
   return (
