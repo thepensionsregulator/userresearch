fc10b16fdfb6280f3c85413557a2cf7eb97029bc
HarryAmmon <harry_a99@hotmail.co.uk>
2020-09-09 18:04:49 +0100

    Create Address context

--- src/components/AddressForm/AddressProvider.tsx
+++ src/components/AddressForm/AddressProvider.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddressContext from './AddressContext';
 
-const AddressProvider = (props: any) => (
-  <AddressContext.Provider value={{}}>{props.children}</AddressContext.Provider>
-);
+const AddressProvider = (props: any) => {
+  const [addressLine1, setAddressLine1] = useState<string>('');
+  const [addressLine2, setAddressLine2] = useState<string>('');
+  const [addressLine3, setAddressLine3] = useState<string>('');
+  const [postTown, setPostTown] = useState<string>('');
+  const [county, setCounty] = useState<string>('');
+  const [postcode, setPostcode] = useState<string>('');
+  const [country, setCountry] = useState<string>('');
+
+  return (
+    <AddressContext.Provider
+      value={{
+        addressLine1,
+        setAddressLine1,
+        addressLine2,
+        setAddressLine2,
+        addressLine3,
+        setAddressLine3,
+        postTown,
+        setPostTown,
+        county,
+        setCounty,
+        postcode,
+        setPostcode,
+        country,
+        setCountry,
+      }}
+    >
+      {props.children}
+    </AddressContext.Provider>
+  );
+};
 
 export default AddressProvider;
