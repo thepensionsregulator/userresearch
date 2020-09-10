73d3070f152df8ca2e14371d937c85f22e4cfac5
HarryAmmon <harry_a99@hotmail.co.uk>
2020-09-10 15:09:42 +0100

    Create Sidebar context

---
+++ src/components/SidebarContextProvider.tsx
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import SidebarContext, { Actions } from './SidebarContext';
+
+const SidebarProvider = (props: any) => {
+  const sectionCompletedState = new Array(8);
+  sectionCompletedState.fill(false);
+
+  const [state, dispatch] = useReducer(
+    (sectionCompletedState: any[], action: Actions) => {
+      switch (action.type) {
+        case 'COMPLETE':
+          sectionCompletedState[action.index] = true;
+          return sectionCompletedState;
+      }
+    },
+    sectionCompletedState,
+  );
+
+  return (
+    <SidebarContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </SidebarContext.Provider>
+  );
+};
+
+export default SidebarProvider;
