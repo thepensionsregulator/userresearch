2ceab8fbdaa1b04a03a39c06f08613df10c42995
HarryAmmon <harry.ammon@tpr.gov.uk>
2021-06-17 09:02:59 +0100

    Refactor to use commas seperation in currency fields

--- src/views/S179/Assets/Assets.tsx
+++ src/views/S179/Assets/Assets.tsx
@@ -1,13 +1,16 @@
 import { Flex, H2, P, Span } from '@tpr/core';
 import { FFInputCurrency, FFInputDate, FFInputNumber } from '@tpr/forms';
 import { HelpLink } from '@tpr/layout';
-import React from 'react';
+import React, { useContext } from 'react';
+import { S179Context } from '../Services/S179Context';
 
 export interface AssetsProps {
   dateOfRelevantAccounts?: Date;
 }
 
 export const Assets: React.FC<AssetsProps> = ({ dateOfRelevantAccounts }) => {
+  const s179Context = useContext(S179Context);
+  const data = s179Context.data;
   return (
     <Flex cfg={{ my: 5, flexDirection: 'column' }}>
       <H2 cfg={{ mb: 5 }}>Assets</H2>
@@ -20,6 +23,7 @@ export const Assets: React.FC<AssetsProps> = ({ dateOfRelevantAccounts }) => {
             hint="This figure should not be reduced by the amount of any external liabilities and should include contract of insurance."
             inputWidth={2}
             required
+            initialValue={data.TotalValue}
           />
 
           <Flex cfg={{ p: 6, bg: 'neutral.2', mt: 9, mb: 8 }}>
@@ -44,6 +48,7 @@ export const Assets: React.FC<AssetsProps> = ({ dateOfRelevantAccounts }) => {
             label="Percentage of assets held as contracts of insurance"
             inputWidth={1}
             cfg={{ mb: 1 }}
+            initialValue={data.PercentageOfAssets}
           />
           <HelpLink title="Help with percentage of assets held as contracts of insurance">
             HelpLink content
